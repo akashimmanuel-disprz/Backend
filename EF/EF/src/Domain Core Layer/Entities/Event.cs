c9ca09277da0f0af24c19f3e1049f8d72b768cd8
Akash Immanuel Solomon <akashimmanuel.s@disprz.com>
2025-09-15 16:14:33 +0530

    Initialized

---
+++ EF/EF/src/Domain Core Layer/Entities/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace EF.Domain_Core_Layer.Entities;
+
+public class Event
+{
+    public int EventId { get; set; }      
+    public string Title { get; set; }     
+    public string Description { get; set; } 
+    public DateTime StartTime { get; set; } 
+    public DateTime EndTime { get; set; }   
+}
